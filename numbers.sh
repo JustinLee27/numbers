e1d7732a9593e4382b931407c79a9542d38e3acc
Justin Lee <justlee@chapman.edu>
2020-11-30 20:27:26 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# numbers
+# Justin Lee
+
+read -p "Enter a positive number: " NUM
+count=1
+while [ $count -le $NUM ]
+do
+	if [ $(($count%2)) -eq 0 ]
+	then
+		echo "$count Even"
+	else
+		echo "$count Odd"
+	fi
+	count=$[$count+1]
+done
+
